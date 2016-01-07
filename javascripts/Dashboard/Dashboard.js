7e1a35f1ef3b08dedbcfd29197590ed3edf192ad
edorble <license@worldsbeyondstudios.com>
2016-01-07 16:20:57 +0100

    added dashboard

---
+++ javascripts/Dashboard/Dashboard.js
@@ -0,0 +1,57 @@
+//Prepare variables
+var myFirebaseUsersRef = 
+	new Firebase("https://edorble-dev.firebaseio.com/users");
+
+//Business Logic
+//General
+// Checks
+function checkAuthState(){
+var authData = myFirebaseUsersRef.getAuth();
+if (authData) {
+  $('#welcome-message').text("Welcome User " + authData.uid);
+} else {
+  $('#welcome-message').text("You need to be logged in for this page");
+}
+}
+
+//Preparation binding
+function bindButtonEvents(){
+$('#button-logout').click( function(){
+	myFirebaseUsersRef.unauth();
+  window.location = "http://cederiks-playground.webflow.io/";
+  });
+}
+
+//View binding
+function bindViewData(){
+	var authData = myFirebaseUsersRef.getAuth();
+  if(authData)
+  {
+  	var user = myFirebaseUsersRef.child(authData.uid);
+    console.log("uid " + authData.uid);
+    console.log("user string " + user.toString());
+    user.once("value", function(data) {
+        var userInfo = data.val();
+        //Bind data to dom
+        $("#placeholder-email").text(userInfo.email);
+        $("#placeholder-world").text(userInfo.world);
+      });
+  }
+  else{
+  	console.log("the user is not logged in.");
+  }
+  
+}
+
+//Test functions
+function runFirebaseExample(){
+//Do nothing
+}
+
+//On page load
+$( document ).ready(function() {
+	checkAuthState();
+	bindButtonEvents();
+  bindViewData();
+	runFirebaseExample();
+});
