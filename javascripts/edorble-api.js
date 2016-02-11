1d6982bea5621de4e96c1ff4e710e82167da7923
edorble <license@worldsbeyondstudios.com>
2016-02-11 21:40:20 +0100

    fix

--- javascripts/edorble-api.js
+++ javascripts/edorble-api.js
@@ -471,7 +471,7 @@ var Edorble =
 				
 			    user.once("value", function(data) {
 			        var userInfo = data.val();
-					console.log(userInfo.worlds);
+					console.log(userInfo.worlds.key());
 			        //Bind data to dom
 			        $(viewWorldCode).text(userInfo.world);
 			      });
