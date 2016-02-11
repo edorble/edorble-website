0fc7a0a2667f37135c4810af8717a75ab42b54c6
edorble <license@worldsbeyondstudios.com>
2016-02-11 21:26:56 +0100

    cleaned dashboard.js

--- javascripts/Dashboard/Dashboard.js
+++ javascripts/Dashboard/Dashboard.js
@@ -1,46 +1,8 @@
-//Prepare variables
-var myFirebaseUsersRef = 
-	new Firebase("https://edorble-dev.firebaseio.com/users");
-
-//Business Logic
-//General
-// Checks
-function checkAuthState(){
-	var authData = myFirebaseUsersRef.getAuth();
-	if (authData) {
-		$('#welcome-message').text("Welcome User " + authData.uid);
-		$("#Requires-login-section").css("display","none"); //Hide the 'you need to login' part
-	} else {
-		$('#welcome-message').text("You need to be logged in for this page");
-		$("#dashboard-section").css("display","none"); //Hide the 'information you can see when logged in' part
-	}
-}
-
 //Preparation binding
 function bindButtonEvents(){
 	//Prepare Logout
 	var idLogoutButton = "#button-logout";
 	Edorble.Logic.Authorisation.prepareLogout(idLogoutButton);
-}
-
-//View binding
-function bindViewData(){
-	var authData = myFirebaseUsersRef.getAuth();
-  if(authData)
-  {
-  	var user = myFirebaseUsersRef.child(authData.uid);
-    console.log("uid " + authData.uid);
-    console.log("user string " + user.toString());
-    user.once("value", function(data) {
-        var userInfo = data.val();
-        //Bind data to dom
-        $("#data-world-code").text(userInfo.world);
-      });
-  }
-  else{
-  	console.log("the user is not logged in.");
-  }
-  
 }
 
 //On page load
@@ -52,6 +14,4 @@ $( document ).ready(function() {
 	//Prepare Logout
 	var idLogoutButton = "#button-logout";
 	Edorble.Logic.Authorisation.prepareLogout(idLogoutButton);
-	
-  	//bindViewData();
 });
