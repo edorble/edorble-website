3673be935f9aa557b39d8339e6433e8a6bb88628
edorble <license@worldsbeyondstudios.com>
2016-02-26 18:17:07 +0100

    fix

--- javascripts/edorble-api.js
+++ javascripts/edorble-api.js
@@ -394,7 +394,7 @@ var Edorble =
 				  	Edorble.Logic.Authorisation.storeNewlyRegisteredUserInformation(authData);
 					
 				  	//Setup that upon login the user is redirected to the following page
-				  	window.location = dashboardpage;
+				  	//window.location = dashboardpage;
   			  }
 			},
 			
